eef69c004665c1f4c9b4f46d5e2ca850a667ce51
Basel Arkia <basel0348@gmail.com>
2019-01-04 22:42:24 +0200

    First Initial Commit

---
+++ src/main/java/com/example/Amdocs/AmdocsApplication.java
@@ -0,0 +1,14 @@
+package com.example.Amdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmdocsApplication.class, args);
+	}
+
+}
+
